92afdb4da830a0d8bf6b56ea20c9784a3b8470b6
Merouane B <merouane@stamp.yt>
2022-01-23 17:59:02 +0100

    Initial Commit

---
+++ Fidel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fidel
+//
+//  Created by Merouane Bellaha on 23/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fidel/FidelApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidelApp.swift
+//  Fidel
+//
+//  Created by Merouane Bellaha on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct FidelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
